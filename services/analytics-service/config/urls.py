34cff277217f1d9acc1f15d3424506af3ad9acab
Nnamu-Radiance <nnamuradiance@gmail.com>
2026-05-26 23:33:43 +0100

    fixing pipeline issues

--- services/analytics-service/config/urls.py
+++ services/analytics-service/config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include
+from . import views
 
 from apps.analytics.views import health_check
 
 urlpatterns = [
+    path('volume/', views.WorkflowVolumeView.as_view(), name='volume'),
     path("admin/", admin.site.urls),
     path("health/", health_check),
     path("metrics/", include("django_prometheus.urls")),
