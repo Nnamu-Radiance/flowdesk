b325fd8a459c20bab47d2ebe74db024f29ecfdb1
Nnamu-Radiance <nnamuradiance@gmail.com>
2026-05-26 23:44:03 +0100

    fix analytics URL import and align analytics tests

--- services/analytics-service/config/urls.py
+++ services/analytics-service/config/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.urls import path, include
-from . import views
 
 from apps.analytics.views import health_check
 
 urlpatterns = [
-    path('volume/', views.WorkflowVolumeView.as_view(), name='volume'),
     path("admin/", admin.site.urls),
     path("health/", health_check),
     path("metrics/", include("django_prometheus.urls")),
